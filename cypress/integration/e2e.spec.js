c7af717e0e93d1aeb5be8321d7ae6e0c8ecf5371
c91608a <marcos.policeno@br.experian.com>
2022-02-25 14:17:16 -0300

    exercicio aula cypress

--- cypress/integration/e2e.spec.js
+++ cypress/integration/e2e.spec.js
@@ -1,5 +1,8 @@
 /// <reference types="cypress" />
 
+import checkoutPage from '../support/page_objects/checkout.page'
+const dadosCheckout = require('../fixtures/cadastro.json')
+
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     /*  Como cliente 
         Quero acessar a Loja EBAC 
@@ -10,12 +13,27 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         E validando minha compra ao final */
 
     beforeEach(() => {
-        cy.visit('/')
+        cy.visit('produtos')
     });
-
-    it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        //TODO 
-    });
-
-
+    it('Deve adicionar a lista de produtos ao carrinho e realizar o checkout', function () {
+        cy.fixture('produtos').then(produtos => {
+            produtos.forEach((produtos, index) => {
+                cy.addProdutos(produtos.produto, produtos.tamanho, produtos.cor, produtos.quantidade)
+            })
+        })
+        checkoutPage.realizaCheckout(
+            dadosCheckout.nome,
+            dadosCheckout.sobrenome,
+            dadosCheckout.empresa,
+            dadosCheckout.pais,
+            dadosCheckout.endereco,
+            dadosCheckout.numero,
+            dadosCheckout.cidade,
+            dadosCheckout.estado,
+            dadosCheckout.cep,
+            dadosCheckout.telefone,
+            dadosCheckout.email)
+            cy.get('.page-title').should('have.text', 'Pedido recebido')
+            cy.get('.woocommerce-notice').contains('Obrigado. Seu pedido foi recebido')
+    })
 })
